22b45a851491a67551d4358ff2718ae38fb12e1a
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-07-29 14:27:44 +0200

    Union types challenge done

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -52,3 +52,32 @@ let requestStatus: "pending" | "success" | "error" = "pending";
 requestStatus = "success";
 requestStatus = "error";
 // requestStatus = "random";
+
+let notSure: any = 4;
+notSure = "Maybe a string instead";
+notSure = false;
+
+let random;
+
+const books = ["1984", "Brave New World", "Fahrenheit 451"];
+
+let foundBook: string | undefined;
+
+for (let book of books) {
+  if (book === "1983") {
+    foundBook = book;
+    break;
+  }
+}
+
+console.log(foundBook?.length);
+
+// Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
+// Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'.
+
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+orderStatus = "shipped";
+orderStatus = "delivered";
+
+let discount: number | string = 20;
+discount = "20%";
