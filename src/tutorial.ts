8abca09d8ee3006c2743a88fdf002db07740c632
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-05 12:28:57 +0200

    Predicate type for narrow down the type and secure it

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1284,3 +1284,34 @@
 // const random = checkInput("2020-05-05");
 // console.log(year);
 // console.log(random);
+
+// type Student = {
+//   name: string;
+//   study: () => void;
+// };
+
+// type User = {
+//   name: string;
+//   login: () => void;
+// };
+
+// type Person = Student | User;
+
+// const randomPerson = (): Person => {
+//   return Math.random() > 0.5
+//     ? { name: "john", study: () => console.log("Studying") }
+//     : { name: "mary", login: () => console.log("Logging in") };
+// };
+
+// const person = randomPerson();
+
+// function isStudent(person: Person): person is Student {
+//   // return "study" in person;
+//   return (person as Student).study !== undefined;
+// }
+
+// if (isStudent(person)) {
+//   person.study;
+// } else {
+//   person.login;
+// }
