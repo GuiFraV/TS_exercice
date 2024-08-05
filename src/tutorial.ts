4348eb1b507a44daa87594aef5280ae2cd3b58c7
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-05 12:16:28 +0200

    instanceof

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1261,3 +1261,26 @@
 // printLength(null);
 // printLength();
 // printLength(undefined);
+
+// try {
+//   // throw new Error("This is an error");
+//   throw "some error";
+// } catch (error) {
+//   if (error instanceof Error) {
+//     console.log(`Caught an Error object : ${error.message}`);
+//   } else {
+//     console.log("unknow error...");
+//   }
+// }
+
+// function checkInput(input: Date | string): string {
+//   if (input instanceof Date) {
+//     return input.getFullYear().toString();
+//   }
+//   return input;
+// }
+
+// const year = checkInput(new Date());
+// const random = checkInput("2020-05-05");
+// console.log(year);
+// console.log(random);
